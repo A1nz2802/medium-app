3200b2954ccffebad0c1608c381dfa88ce9c981d
Brawer Nuñez <n.eedar@hotmail.com>
2022-02-07 11:38:59 -0500

    Feature Image & sanity config complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -11,7 +11,19 @@ export default function Home() {
 
       <Header />
 
-      <h1>This is medium 2.0</h1>
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className='px-10 space-y-5'>
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">Medium</span> is a place to write, read, and connect</h1>
+          <h2>It's easy and free to post your thinking on my topic and connect with millions of readers.</h2>
+        </div>
+        
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full" 
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+      </div>
+
+      {/* Posts */}
     </div>
   )
 }
