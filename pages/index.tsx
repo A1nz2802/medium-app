3af10e4807aaea5be1ea1516ff1f9d9500bc8a49
Brawer Nuñez <n.eedar@hotmail.com>
2022-02-08 23:26:50 -0500

    dynamic routes & comments section complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,6 +61,7 @@ export default function Home({ posts }: Props) {
           </Link>
         ))}
       </div>
+
     </div>
   )
 }
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,13 +3,23 @@ import { Post } from '../../typings';
 import { sanityClient, urlFor } from '../../sanity';
 import { GetStaticProps } from 'next';
 import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
 interface Props {
   post: Post;
 }
 
 const Post = ({ post }: Props) => {
-  console.log( post );
+  
+  const { register, handleSubmit, formState:{ errors } } = useForm<IFormInput>();
+
   return(
     <main>
       <Header />
@@ -36,7 +46,75 @@ const Post = ({ post }: Props) => {
           </p>
 
         </div>
+
+        <div className='mt-10'>
+          <PortableText 
+            content={ post.body }
+            dataset={ process.env.NEXT_PUBLIC_SANITY_DATASET! }
+            projectId={ process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            serializers={
+              {
+                h1: ( { children }: any ) => (
+                  <h1 className="text-2xl font-bold my-5">{ children }</h1>
+                ),
+                h2: ( { children }: any ) => (
+                  <h2 className="text-xl font-bold my-5">{ children }</h2>
+                ),
+                li: ( { children }: any ) => (
+                  <li className="ml-4 list-disc">{ children }</li>
+                ),
+                link: ({ href, children }: any ) => (
+                  <a href={ href } className="text-blue-500 hover:underline">{ children }</a>
+                ),
+                p: ( { children }: any ) => (
+                  <p className="ml-4 list-disc">{ children }</p>
+                ),
+              }}
+          />
+        </div>
       </article>
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500"/>
+      
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+
+        
+        
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4> 
+        <hr className="py-3 mt-2"/>
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input 
+            className="shadow border rounded py-2 px-3 form-input
+            mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+            placeholder="John Appleseed" 
+            type="text" 
+          />
+        </label>
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input 
+            className="shadow border rounded py-2 px-3 form-input
+            mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+            placeholder="John Appleseed" 
+            type="text" 
+          />
+        </label>
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea 
+            className="shadow border rounded py-3 px-3 form-textarea
+            mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+            placeholder="John Appleseed" 
+            rows={ 8 } 
+          />
+        </label>
+      </form>
+
     </main>
   );
 };
