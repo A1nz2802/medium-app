bbeb123aac8b197cf4eb6e392f6968ec73d48c2c
Brawer Nuñez <n.eedar@hotmail.com>
2022-02-09 23:35:00 -0500

    render comments in post section

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -174,7 +174,22 @@ const Post = ({ post }: Props) => {
         </form>
 
       )}
-      
+
+      {/* Comments */}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2"/>
+
+        { post.comments.map( ({ _id:id, name, comment }) => (
+          <div key={ id }>
+            <p>
+              <span className="text-yellow-400">{ name }: </span>{ comment }
+            </p>
+          </div>
+        ))}
+
+      </div>
+
     </main>
   );
 };
